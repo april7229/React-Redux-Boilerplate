67694249c332a11ee995198b633acc96e7f8af74
april <martinezapril56@yahoo.com>
2018-12-08 17:01:32 -0500

    redux

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,33 +1,32 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
+export default function(){
+    return[
+        {
+           id:1,
+           first:"april",
+           last: "Martinez",
+           age: 71,
+           description: "Bucky is a react developer and youTuber",
+           thumbnail: "http://i.imgur.com/7yUvePI.jpg"
 
-export default function () {
-    return [
-        {
-            id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
         },
         {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
+            id:2,
+            first:"charles",           
+            last:"Martinez",
+            age: 72,
+            description:"charles is a cab driver",
+            thumbnail:"http://i.imgur.com/52x"
+
+          
         },
         {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
+        id:3,
+        first:"charles",           
+        last:"Martinez",
+        age: 72,
+        description:"charles is a cab driver",
+        thumbnail:"http://i.imgur.com/52x"
         }
     ]
+
 }
